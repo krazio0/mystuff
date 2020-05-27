43c1c59f9536d0db9cdccdf088b959ba4bff7f02
krazio0 <66001285+krazio0@users.noreply.github.com>
2020-05-27 10:13:28 +0300

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,10 @@
+function solve(input){
+    let number = input.shift();
+    if( number == "s3cr3t!P@ssw0rd" ) {
+        console.log('Welcome') }
+     else {   
+    console.log("Wrong password!") }
+
+}
+
+solve(["s3cr3tP@ssw0rd"])
